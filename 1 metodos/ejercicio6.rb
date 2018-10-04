0d7a9db14779513d74791ebfe698a14a0436d40c
Zamiz <zamiztobar@gmail.com>
2018-10-03 23:53:12 -0300

    +Ejercicio6

--- 1 metodos/ejercicio6.rb
+++ 1 metodos/ejercicio6.rb
@@ -7,12 +7,12 @@
 # *****
 
 def draw_line(size)
-  '*' * size
+  puts '*' * size
 end
 
 def draw_lines(size)
-  size.times { }
+  size.times {print '*' }
 end
 
-draw_line 2
-draw_lines 10
+draw_line(2)
+draw_lines(10)
