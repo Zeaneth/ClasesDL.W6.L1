a2d4a0fb0ef98ad2bb608c5fbd20e0660901b38d
Zamiz <zamiztobar@gmail.com>
2018-10-07 19:26:33 -0300

    +Ejercicio 4

--- 2 arrays/ejercicio4.rb
+++ 2 arrays/ejercicio4.rb
@@ -6,11 +6,13 @@ Se tiene un arreglo de productos, se pide que el output sea:
 <div class='product'><p> Producto4 </p></div>
 =end
 
+# Fuente = https://stackoverflow.com/questions/1274675/what-does-warray-mean (es para omitir las comillas y comas) 
 products = %w(Producto1 Producto2 Producto3 Producto4)
 
 html = ''
 products.each do |i|
   html += "<div class='product'>"
+  html += " #{products.at(products.index(i))} "
   html += "</div>\n"
 end
 
